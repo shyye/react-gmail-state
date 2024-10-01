64d3644116d756b028a10bd2f75cdc73458e125e
shyye <emma.art.dev@gmail.com>
2024-10-01 11:12:32 +0200

    Extension 1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,41 @@ function App() {
   // Use initialEmails for state
   console.log(initialEmails);
   const [emails, setEmails] = useState(initialEmails);
-  console.log(emails);
+  // console.log(emails);
+
+  // TODO: How does functions here automatically have an event if no parameter is given?
+  function toggleRead(emailId, isRead) {
+    // Copy to update object the correct way: https://react.dev/learn/updating-arrays-in-state#updating-objects-inside-arrays
+    setEmails(
+      emails.map((email) => {
+        if (email.id === emailId) {
+          // Create a *new* object with changes
+          return { ...email, read: !isRead };
+        } else {
+          // No changes
+          return email;
+        }
+      })
+    );
+  }
+
+  function toggleStar(emailId, isStarred) {
+    // Copy to update object the correct way: https://react.dev/learn/updating-arrays-in-state#updating-objects-inside-arrays
+
+    // TODO: Duplicated code, refactor?
+
+    setEmails(
+      emails.map((email) => {
+        if (email.id === emailId) {
+          // Create a *new* object with changes
+          return { ...email, starred: !isStarred };
+        } else {
+          // No changes
+          return email;
+        }
+      })
+    );
+  }
 
   return (
     <div className="app">
@@ -44,14 +78,26 @@ function App() {
       <main className="emails">
         {
           /* Render a list of emails here */
-
+          // Reference, event with parameter: https://stackoverflow.com/a/42597619
           emails.map((email) => (
-            <li key={email.id} className="email">
+            // <li key={email.id} className="email">
+            <li key={email.id} className={email.read ? "email read" : "email"}>
               <div className="select">
-                <input className="select-checkbox" type="checkbox" />
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  checked={email.read ? true : false}
+                  // TODO: What is the correct way to do this? Pass parameters like this or read the 'key' value from parent element
+                  onChange={() => toggleRead(email.id, email.read)}
+                />
               </div>
               <div className="star">
-                <input className="star-checkbox" type="checkbox" />
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred ? true : false}
+                  onChange={() => toggleStar(email.id, email.starred)}
+                />
               </div>
               <div className="sender">{email.sender}</div>
               <div className="title">{email.title}</div>
